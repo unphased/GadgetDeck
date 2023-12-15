cebaf25ff29f2784aafd0df03c271c4390e51fa0
Steven Lu (steam deck[tmux]) <stevenlu443@gmail.com>
2023-12-15 12:33:16 -0500

    - old bugs fixed - new bugs added

--- build.sh
+++ build.sh
@@ -24,4 +24,11 @@ mkdir -p /home/deck/.steam/steam/controller_config
 cp util/game_actions_480.vdf /home/deck/.steam/steam/controller_config/
 
 chmod +x gadget-deck-manager.py
+python -m venv venv && venv/bin/pip install -r requirements.txt
+sudo cp util/99-gadget-deck.rules /etc/polkit-1/rules.d/
+
+# bring the fixed service units in from repo
+echo Running: cp "${0%/*}"/util/gadget-deck*.service util/
+cp "${0%/*}"/util/gadget-deck*.service util/
+
 
