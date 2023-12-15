eb93a16f2bead8444b2157708b296f0bedad9707
Steven Lu (steam deck) <stevenlu443@gmail.com>
2023-12-15 12:25:53 -0500

    - old bugs fixed - new bugs added

--- build.sh
+++ build.sh
@@ -16,7 +16,12 @@ make # This runs the setup make target
 # INSTALL_DIR="/usr/share/gadget-deck"
 # sudo cp -r GadgetDeck ${INSTALL_DIR}/
 chmod +x GadgetDeck/GadgetDeck
+
+# need this to be run from the desktop mode!
 steamos-add-to-steam $(pwd)/GadgetDeck/GadgetDeck
 
 mkdir -p /home/deck/.steam/steam/controller_config
 cp util/game_actions_480.vdf /home/deck/.steam/steam/controller_config/
+
+chmod +x gadget-deck-manager.py
+
